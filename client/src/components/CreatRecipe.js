0008f2e1087e14bec3b1ab97221cc98e9cca2840
bonny Monah <bonnymonah.bm@gmail.com>
2024-09-26 18:22:37 +0300

    new commit

---
+++ client/src/components/CreatRecipe.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const CreateRecipe=()=>{
+    return(
+        <div className="home">
+            <h1>CreateRecipe</h1>
+        </div>
+    )
+}
+
+export default CreateRecipe
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
             </button>
             
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
+                <ul className="navbar-nav me-auto">
                     <li className="nav-item active">
                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
@@ -31,8 +31,9 @@ const NavBar = () => {
                         <a className="nav-link disabled" href="#">Disabled</a>
                     </li>
                 </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                {/* Use ms-auto in Bootstrap 5 */}
+                <form className="form-inline d-flex ms-auto my-2 my-lg-0">
+                    <input className="form-control me-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
